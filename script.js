388176a6abd24343019706cfffdbfc46834e290e
Franco <franco_t92@hotmail.com>
2024-06-06 21:43:27 -0400

    done first modal

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const servicesButton = document.querySelector("#servicesButton");
 const dogServices = document.querySelector("#dog__services");
+const closeButtonModal = dogServices.querySelector("#close__button");
 
 servicesButton.addEventListener("click", () => {
   dogServices.classList.add("services__opened");
 });
 
+closeButtonModal.addEventListener("click", () => {
+  dogServices.classList.remove("services__opened");
+});
