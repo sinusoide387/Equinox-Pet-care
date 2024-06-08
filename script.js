f92630391ec4bad35c41e0372695317e52cd88cb
Franco <franco_t92@hotmail.com>
2024-06-08 10:17:27 -0400

    working on aboutus section

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ servicesButton.addEventListener("click", () => {
 closeButtonModal.addEventListener("click", () => {
   dogServices.classList.remove("services__opened");
 });
+
+///////////////////////////////////////////////////////////////
+
+const aboutUsText = {
+  description: "We are a company fully dedicated to pets since 1992.",
+};
+
+const aboutUsSection = document.querySelector("#info__section_about-us");
+console.log(aboutUsSection);
+const textArea = aboutUsSection.querySelector(".about__us_text-area");
+console.log(textArea);
+textArea.textContent = aboutUsText.description;
